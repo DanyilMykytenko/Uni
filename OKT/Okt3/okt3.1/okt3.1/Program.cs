250eaae44f9554f00eab307fed610a461eb5da63
Daniyl <Danyil.1488@gmail.com>
2020-11-24 21:13:06 +0200

    Frontier#2 done

--- OKT/Okt3/okt3.1/okt3.1/Program.cs
+++ OKT/Okt3/okt3.1/okt3.1/Program.cs
@@ -16,14 +16,16 @@ namespace okt3._1
             for (int index = 0; index < 30; index++)
             {
                 Array[index] = rnd.Next(0, 30);
+                Console.Write("{0} ", Array[index]);
             }
             for (int index = 0; index < 30; index++)
             {
                 if (Array[index] < Math.Pow(index, 2))
                 {
                     sum += Array[index];
-                }       
+                }
             }
+            Console.WriteLine("\n");
             Console.WriteLine(sum);
             Console.ReadLine();
         }
