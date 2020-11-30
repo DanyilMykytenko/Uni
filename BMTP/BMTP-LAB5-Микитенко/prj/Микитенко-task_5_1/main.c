45b8ef21b54e84ea6300628f66c8244694975a9e
Daniyl <Danyil.1488@gmail.com>
2020-11-30 17:26:29 +0200

    Lab 5 BMTP was fixed

--- BMTP/BMTP-LAB5-Микитенко/prj/Микитенко-task_5_1/main.c
+++ BMTP/BMTP-LAB5-Микитенко/prj/Микитенко-task_5_1/main.c
@@ -1,82 +1,115 @@
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
     int main()
     {
         char *locale = setlocale(LC_ALL, "");
-        int AmountDecember, DeregisteredNow, RegisteredNow, BornNow, DiedNow, AmountNow, Diff;
-        char display[11];
+        unsigned int AmountDecember, DeregisteredNow, RegisteredNow, BornNow, DiedNow, AmountNow, Diff;
+        char display[1];
         printf("ќбер≥ть мову ≥нтерфейсу:\nChoose the language of the display:\n(у/e)\n");
         gets(display);
-        switch(display[0])
+        while(0 < 1)
         {
-            case('y'):
-                printf("¬вед≥ть загальну чисельн≥сть населенн€ област≥ станом на 1 с≥чн€ ц.р.: ");
-                scanf("%d", &AmountDecember);
-                printf("¬вед≥ть к≥льк≥сть громад€н, €к≥ станом на 1 с≥чн€ поточного м≥с€ц€ ц.р. вибули: ");
-                scanf("%d", &DeregisteredNow);
-                printf("¬вед≥ть к≥льк≥сть громад€н, €к≥ станом на 1 число поточного м≥с€ц€ ц.р. прибули: ");
-                scanf("%d", &RegisteredNow);
-                printf("¬вед≥ть к≥льк≥сть громад€н, €к≥ станом на 1 число поточного м≥с€ц€ ц.р. народилис€: ");
-                scanf("%d", &BornNow);
-                printf("¬вед≥ть к≥льк≥сть громад€н, €к≥ станом на 1 число поточного м≥с€ц€ ц.р. померли: ");
-                scanf("%d", &DiedNow);
-                AmountNow = AmountDecember - DeregisteredNow + RegisteredNow + BornNow - DiedNow;
-                if(AmountNow < 0 || AmountDecember < 0)
+            switch(display[0])
+            {
+                case('y'):
+                    printf("ѕризначенн€ алгоритму - виведенн€ чисельност≥ населенн€ област≥ станом на 1 число поточного м≥с€ц€ та пов≥домити про зменшенн€ або зб≥льшенн€ к≥лькост≥ мешканц≥в.\n");
+                    printf("ѕ≤Ѕ розробника - ћикитенко ƒанињл ёр≥йович.\n");
+                    printf("—тудент ÷ентрального нац≥онального техн≥чного ун≥верситету.\n");
+                    printf("–≥к розробки - 2020.\n");
+                    printf("¬вед≥ть загальну чисельн≥сть населенн€ област≥ станом на 1 с≥чн€ ц.р.: ");
+                    scanf("%d", &AmountDecember);
+                    printf("¬вед≥ть к≥льк≥сть громад€н, €к≥ станом на 1 с≥чн€ поточного м≥с€ц€ ц.р. вибули: ");
+                    scanf("%d", &DeregisteredNow);
+                    printf("¬вед≥ть к≥льк≥сть громад€н, €к≥ станом на 1 число поточного м≥с€ц€ ц.р. прибули: ");
+                    scanf("%d", &RegisteredNow);
+                    printf("¬вед≥ть к≥льк≥сть громад€н, €к≥ станом на 1 число поточного м≥с€ц€ ц.р. народилис€: ");
+                    scanf("%d", &BornNow);
+                    printf("¬вед≥ть к≥льк≥сть громад€н, €к≥ станом на 1 число поточного м≥с€ц€ ц.р. померли: ");
+                    scanf("%d", &DiedNow);
+                    AmountNow = AmountDecember - DeregisteredNow + RegisteredNow + BornNow - DiedNow;
+                    if(AmountNow < 0 || AmountDecember < 0)
+                        {
+                            printf("ѕомилка: були неправильно введен≥ дан≥...");
+                            return 0;
+                        }
+                    if(AmountNow > AmountDecember)
+                        {
+                            Diff = AmountNow - AmountDecember;
+                            printf("\n ≥льк≥сть мешканц≥в на зараз складаЇ: %d", AmountNow);
+                            printf("\n ≥льк≥сть мешканц≥в област≥ у пер≥од з 1 с≥чн€ ц.р. до 1 числа поточного м≥с€ц€ ц.р. зб≥льшилось на %d\n", Diff);
+                        }
+                    if (AmountNow < AmountDecember)
+                        {
+                            Diff = AmountDecember - AmountNow;
+                            printf("\n ≥льк≥сть мешканц≥в на зараз складаЇ: %d", AmountNow);
+                            printf("\n ≥льк≥сть мешканц≥в област≥ у пер≥од з 1 с≥чн€ ц.р. до 1 числа поточного м≥с€ц€ ц.р. зменшилась на %d\n", Diff);
+                        }
+                    if (AmountNow == AmountDecember)
+                        printf("\n ≥льк≥сть мешканц≥в не зм≥нилась.\n");
+                    printf("\n¬и бажаЇте завершити виконанн€ програми? (“/Ќ)\n");
+                    gets(display);
+                    switch(display[0])
                     {
-                        printf("ѕомилка: були неправильно введен≥ дан≥...");
-                        return 0;
+                        case 'T':
+                            getch();
+                            return 0;
+                        case 'H':
+                            getch();
+                            break;
                     }
-                if(AmountNow > AmountDecember)
+                    break;
+                case('e'):
+                    printf("Assigned to the algorithm is the increase in the number of population of the region by the 1st number of the flow month and, in turn, the increase in the number of bags.\n");
+                    printf("Name of the developer - Mykytenko Daniil Yuriyovych.\n");
+                    printf("Student of the Central National Technical University.\n");
+                    printf("Year of development - 2020.\n");
+                    printf("Enter the total population of the region as of January 1 this year.: ");
+                    scanf("%d", &AmountDecember);
+                    printf("Enter the number of citizens who, as of January 1 of the current month of this year, was deregestered: ");
+                    scanf("%d", &DeregisteredNow);
+                    printf("Enter the number of citizens who, as of January 1 of the current month of this year, was regestered: ");
+                    scanf("%d", &RegisteredNow);
+                    printf("Enter the number of citizens who, as of January 1 of the current month of this year, was born: ");
+                    scanf("%d", &BornNow);
+                    printf("Enter the number of citizens who, as of January 1 of the current month of this year, died: ");
+                    scanf("%d", &DiedNow);
+                    AmountNow = AmountDecember - DeregisteredNow + RegisteredNow + BornNow - DiedNow;
+                    if(AmountNow < 0 || AmountDecember < 0)
+                        {
+                            printf("Error: wrong input...");
+                            return 0;
+                        }
+                    if(AmountNow > AmountDecember)
+                        {
+                            Diff = AmountNow - AmountDecember;
+                            printf("\nThe number of inhabitants is currently: %d", AmountNow);
+                            printf("\nThe number of residents of the region in the period from January 1 this year until the 1st of the current month this year increased by %d", Diff);
+                        }
+                    if (AmountNow < AmountDecember)
+                        {
+                            Diff = AmountDecember - AmountNow;
+                            printf("\nThe number of inhabitants is currently: %d", AmountNow);
+                            printf("\nThe number of residents of the region in the period from January 1 this year until the 1st of the current month this year decreased by %d", Diff);
+                        }
+                    if (AmountNow == AmountDecember)
+                        printf("\nThe number of inhabitants has not changed.");
+                    break;
+                    printf("\nDo you want to exit from the programm? (Y/N)\n");
+                    gets(display);
+                    switch(display[0])
                     {
-                        Diff = AmountNow - AmountDecember;
-                        printf("\n ≥льк≥сть мешканц≥в на зараз складаЇ: %d", AmountNow);
-                        printf("\n ≥льк≥сть мешканц≥в област≥ у пер≥од з 1 с≥чн€ ц.р. до 1 числа поточного м≥с€ц€ ц.р. зб≥льшилось на %d", Diff);
+                        case 'Y':
+                            getch();
+                            return 0;
+                        case 'N':
+                            getch();
+                            break;
                     }
-                if (AmountNow < AmountDecember)
-                    {
-                        Diff = AmountDecember - AmountNow;
-                        printf("\n ≥льк≥сть мешканц≥в на зараз складаЇ: %d", AmountNow);
-                        printf("\n ≥льк≥сть мешканц≥в област≥ у пер≥од з 1 с≥чн€ ц.р. до 1 числа поточного м≥с€ц€ ц.р. зменшилась на %d", Diff);
-                    }
-                if (AmountNow == AmountDecember)
-                    printf("\n ≥льк≥сть мешканц≥в не зм≥нилась.");
-                break;
-            case('e'):
-                printf("Enter the total population of the region as of January 1 this year.: ");
-                scanf("%d", &AmountDecember);
-                printf("Enter the number of citizens who, as of January 1 of the current month of this year, was deregestered: ");
-                scanf("%d", &DeregisteredNow);
-                printf("Enter the number of citizens who, as of January 1 of the current month of this year, was regestered: ");
-                scanf("%d", &RegisteredNow);
-                printf("Enter the number of citizens who, as of January 1 of the current month of this year, was born: ");
-                scanf("%d", &BornNow);
-                printf("Enter the number of citizens who, as of January 1 of the current month of this year, died: ");
-                scanf("%d", &DiedNow);
-                AmountNow = AmountDecember - DeregisteredNow + RegisteredNow + BornNow - DiedNow;
-                if(AmountNow < 0 || AmountDecember < 0)
-                    {
-                        printf("Error: wrong input...");
-                        return 0;
-                    }
-                if(AmountNow > AmountDecember)
-                    {
-                        Diff = AmountNow - AmountDecember;
-                        printf("\nThe number of inhabitants is currently: %d", AmountNow);
-                        printf("\nThe number of residents of the region in the period from January 1 this year until the 1st of the current month this year increased by %d", Diff);
-                    }
-                if (AmountNow < AmountDecember)
-                    {
-                        Diff = AmountDecember - AmountNow;
-                        printf("\nThe number of inhabitants is currently: %d", AmountNow);
-                        printf("\nThe number of residents of the region in the period from January 1 this year until the 1st of the current month this year decreased by %d", Diff);
-                    }
-                if (AmountNow == AmountDecember)
-                    printf("\nThe number of inhabitants has not changed.");
-                break;
-            default:
-                break;
+                default:
+                    break;
+            }
         }
-        return 0;
-    }
+    return 0;
+}
